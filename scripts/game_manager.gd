8980dfca78e91df33ff40f8e84e42f645f023f5b
Jack Schonherr <jack.schonherr@gmail.com>
2024-11-27 21:50:46 -0800

    basic level switching

---
+++ scripts/game_manager.gd
@@ -0,0 +1,18 @@
+class_name GameManager
+extends Node
+
+
+var level_1:PackedScene = preload("res://scenes/test_level_1.tscn")
+var level_2:PackedScene = preload("res://scenes/test_level_2.tscn")
+var cur_scene = 1
+
+
+func _process(delta: float) -> void:		
+	# demo to switch between levels
+	if Input.is_action_just_pressed("ui_accept"):
+		if cur_scene == 1:
+			get_tree().change_scene_to_packed(level_2)
+			cur_scene = 2
+		else:
+			get_tree().change_scene_to_packed(level_1)
+			cur_scene = 1
