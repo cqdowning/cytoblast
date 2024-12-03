45a7370d97b1fc23380c0315a61b4dc2f70d059e
Jack <jack.schonherr@gmail.com>
2024-12-02 21:32:48 -0800

    working demo

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,8 @@ class_name GameManager
 extends Node
 
 
+signal room_over(room_id:int)
+
 var level_1:PackedScene = preload("res://scenes/demo/demo_level_1.tscn")
 var enemies_remaining = 0
 var room_active = false
@@ -9,11 +11,18 @@ var room_id = 0
 
 
 func _process(_delta: float) -> void:		
-	# demo to switch between levels
+	# switch to the room demo
 	if Input.is_action_just_pressed("start_demo"):
 		get_tree().change_scene_to_packed(level_1)
+	# kill enemy in demo
+	if Input.is_action_just_pressed("kill _enemy"):
+		enemies_remaining -= 1
+		print("enemies remaining: ", enemies_remaining)
 			
-		
+	if room_active and enemies_remaining == 0:
+		room_over.emit(room_id)	
+		room_active = false
 			
 func change_level(next_level:PackedScene):
+	room_id = 0
 	get_tree().change_scene_to_packed(next_level)
--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -10,8 +10,17 @@ var next_level: PackedScene
 
 func _ready() -> void:
 	player.position = spawn_point.position
+	game_manager.room_over.connect(_on_room_over)
 
 
 func _on_level_end_gate_body_entered(body: Node2D) -> void:
 	if body is Player:
 		game_manager.change_level(next_level)
+
+
+func _on_room_over(room_id:int):
+	# remove the correct door depending on the room we are in
+	var door_to_delete = "RoomDoor" + str(game_manager.room_id)
+	for child in get_children():
+		if child.name == door_to_delete:
+			child.queue_free()
