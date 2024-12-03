62e10de5607f0369922f6105a8aa7041f0aa120f
Jack <jack.schonherr@gmail.com>
2024-12-02 21:51:26 -0800

    comments

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -11,18 +11,20 @@ var room_id = 0
 
 
 func _process(_delta: float) -> void:		
-	# switch to the room demo
+	# switch to the room demo (keybind is "9")
 	if Input.is_action_just_pressed("start_demo"):
 		get_tree().change_scene_to_packed(level_1)
-	# kill enemy in demo
+	# kill enemy in demo (keybind is "0")
 	if Input.is_action_just_pressed("kill _enemy"):
 		enemies_remaining -= 1
 		print("enemies remaining: ", enemies_remaining)
 			
+	# emit signal once the room is complete
 	if room_active and enemies_remaining == 0:
 		room_over.emit(room_id)	
 		room_active = false
 			
 func change_level(next_level:PackedScene):
+	# reset the room id, each level's rooms will be indexed starting at 1
 	room_id = 0
 	get_tree().change_scene_to_packed(next_level)
--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -9,11 +9,14 @@ var next_level: PackedScene
 
 
 func _ready() -> void:
+	# initialize the player. in the future this will include setting
+	# the health, inventory, etc.
 	player.position = spawn_point.position
 	game_manager.room_over.connect(_on_room_over)
 
 
 func _on_level_end_gate_body_entered(body: Node2D) -> void:
+	# switch to the next level once the player collides with the trigger
 	if body is Player:
 		print("changing levels")
 		game_manager.change_level(next_level)
