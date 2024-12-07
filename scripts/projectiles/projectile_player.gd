65081a5a18570b76e14dd29f9cfdd41a3714dd2f
cqdowning <cqdowning@gmail.com>
2024-12-07 15:44:32 -0800

    Player projectiles hit enemies

--- scripts/projectiles/projectile_player.gd
+++ scripts/projectiles/projectile_player.gd
@@ -13,8 +13,8 @@ func _physics_process(delta):
 
 func _on_body_entered(body: Node2D):
 	super(body)
-	# Check if we hit a player
-	if body.is_in_group("player") and body.has_method("take_damage"):
+	# Check if we hit an enemy
+	if body.is_in_group("enemies") and body.has_method("take_damage"):
 		body.take_damage(damage)
 	
 	# Destroy the projectile on any collision except the player
