7ac904d6eafab4b7b96002f0533155c5c03bf9f1
Jack <jack.schonherr@gmail.com>
2024-12-07 20:44:39 -0800

    implemented damage engine

--- scripts/damage_engine.gd
+++ scripts/damage_engine.gd
@@ -2,6 +2,29 @@ class_name DamageEngine
 extends Node
 
 
-static func damage_multiplier(projectile:Projectile, enemy:Enemy):
-	return 1
+static func damage_multiplier(projectile:Projectile, enemy:Enemy) -> float:
+	if projectile.type == Weapon.Type.ANTIBIOTIC:
+		if enemy.type == Enemy.Type.BACTERIA:
+			return 2.0
+		elif enemy.type == Enemy.Type.VIRUS:
+			return 0.5
+		elif enemy.type == Enemy.Type.PARASITE:
+			return 1.0
+	elif projectile.type == Weapon.Type.ANTIVIRAL:
+		if enemy.type == Enemy.Type.BACTERIA:
+			return 1.0
+		elif enemy.type == Enemy.Type.VIRUS:
+			return 2.0
+		elif enemy.type == Enemy.Type.PARASITE:
+			return 0.5
+	elif projectile.type == Weapon.Type.ANTIPARASITIC:
+		if enemy.type == Enemy.Type.BACTERIA:
+			return 0.5
+		elif enemy.type == Enemy.Type.VIRUS:
+			return 1.0
+		elif enemy.type == Enemy.Type.PARASITE:
+			return 2.0
+			
+	# return 1.0 if either type is NONE	
+	return 1.0
 	
