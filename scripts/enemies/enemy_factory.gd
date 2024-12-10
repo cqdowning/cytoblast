b9de2fd8d76a0e44c58b56188136af04c1244b71
Jack <jack.schonherr@gmail.com>
2024-12-10 13:26:07 -0800

    enemy spawners use enemy specs

--- scripts/enemies/enemy_factory.gd
+++ scripts/enemies/enemy_factory.gd
@@ -3,4 +3,13 @@ extends Node
 
 
 static func build(spec:EnemySpec):
-	pass
+	var enemy_scene
+	var enemy
+	match spec.type:
+		EnemySpec.Type.BACTERIA:
+			enemy_scene = load("res://scenes/enemies/shooter.tscn")
+		_:
+			enemy_scene = load("res://scenes/enemies/test_enemy.tscn")
+	enemy = enemy_scene.instantiate() as Enemy
+	enemy.weapon_drops = spec.weapon_drops
+	return enemy
