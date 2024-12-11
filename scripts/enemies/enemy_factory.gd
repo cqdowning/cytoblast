d1bec57086a0583e16a2357fd34755ba307a8148
corypham <corypham1@gmail.com>
2024-12-10 20:31:29 -0800

    Enemny factory update

--- scripts/enemies/enemy_factory.gd
+++ scripts/enemies/enemy_factory.gd
@@ -2,12 +2,16 @@ class_name EnemyFactory
 extends Node
 
 
-static func build(spec:EnemySpec):
+static func build(spec: EnemySpec):
 	var enemy_scene
 	var enemy
 	match spec.type:
 		EnemySpec.Type.BACTERIA:
 			enemy_scene = load("res://scenes/enemies/shooter.tscn")
+		EnemySpec.Type.VIRUS:
+			enemy_scene = load("res://scenes/enemies/turret.tscn")
+		EnemySpec.Type.PARASITE:
+			enemy_scene = load("res://scenes/enemies/biter.tscn")
 		_:
 			enemy_scene = load("res://scenes/enemies/test_enemy.tscn")
 	enemy = enemy_scene.instantiate() as Enemy
