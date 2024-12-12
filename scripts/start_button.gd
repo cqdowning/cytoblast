cfa3469c756884165a65db422b355801d28c09f5
qjb13 <thebrodericks5@gmail.com>
2024-12-12 13:45:28 -0800

    added start button

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -4,7 +4,18 @@ var starting_level: PackedScene = preload("res://scenes/levels/level_1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var style = StyleBoxFlat.new()
+	style.bg_color = Color("#cb62ed", 0)  # Change to your desired color
+	style.corner_radius_top_left = 8    # Optional: rounded corners
+	style.corner_radius_top_right = 8
+	style.corner_radius_bottom_left = 8
+	style.corner_radius_bottom_right = 8
+	
+	var hover = StyleBoxFlat.new()
+	hover.bg_color = Color("#cb62ed", 0.25)  # Change to your desired color
+	# Apply the style to normal state
+	add_theme_stylebox_override("normal", style)
+	add_theme_stylebox_override("hover", hover)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
