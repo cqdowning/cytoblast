d9544edb5d389f32f7e2640cfd8e411a3c6b51a2
Noor A. <55614986+noor-ashour@users.noreply.github.com>
2024-12-13 13:41:34 -0800

    Modify main menu look, add game title sprite

--- scripts/credits_button.gd
+++ scripts/credits_button.gd
@@ -2,18 +2,20 @@ extends Button
 
 
 func _ready():
-	var style: StyleBoxFlat = StyleBoxFlat.new()
-	style.bg_color = Color("#cb62ed", 0)  # Change to your desired color
-	style.corner_radius_top_left = 8    # Optional: rounded corners
-	style.corner_radius_top_right = 8
-	style.corner_radius_bottom_left = 8
-	style.corner_radius_bottom_right = 8
-	
-	var hover: StyleBoxFlat = StyleBoxFlat.new()
-	hover.bg_color = Color("#cb62ed", 0.25)  # Change to your desired color
-	# Apply the style to normal state
-	add_theme_stylebox_override("normal", style)
-	add_theme_stylebox_override("hover", hover)
+	#
+	#var style: StyleBoxFlat = StyleBoxFlat.new()
+	#style.bg_color = Color("#cb62ed", 0)  # Change to your desired color
+	#style.corner_radius_top_left = 8    # Optional: rounded corners
+	#style.corner_radius_top_right = 8
+	#style.corner_radius_bottom_left = 8
+	#style.corner_radius_bottom_right = 8
+	#
+	#var hover: StyleBoxFlat = StyleBoxFlat.new()
+	#hover.bg_color = Color("#cb62ed", 0.25)  # Change to your desired color
+	## Apply the style to normal state
+	#add_theme_stylebox_override("normal", style)
+	#add_theme_stylebox_override("hover", hover)
+	pass
 
 func _on_pressed():
 	# jump to scene level_1.tscn
