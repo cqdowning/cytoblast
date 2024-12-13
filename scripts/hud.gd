2697180a2ff22107ce960789c6b0f78c9a28d5e4
cqdowning <cqdowning@gmail.com>
2024-12-13 00:37:13 -0800

    Added weapons on the side

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -8,6 +8,9 @@ extends Control
 @onready var rifle_label = $Inventory/WeaponsContainer/Rifle
 @onready var shotgun_label = $Inventory/WeaponsContainer/Shotgun
 @onready var machinegun_label = $Inventory/WeaponsContainer/MachineGun
+@onready var slot_0 = $Weapons/Slot0
+@onready var slot_1 = $Weapons/Slot1
+@onready var slot_2 = $Weapons/Slot2
 @onready var game_manager = get_node("/root/game_manager")
 
 # Store weapon information
@@ -29,6 +32,7 @@ func connect_player_signals():
 	game_manager.weapon_switched.connect(_on_weapon_switched)
 	game_manager.ammo_changed.connect(_on_ammo_shot)
 	game_manager.weapon_dropped.connect(_on_weapon_dropped)
+	current_inventory.weapon_added.connect(_on_weapon_added)
 
 func _on_health_changed(current_health, max_health):
 	# Update health bar
@@ -45,16 +49,52 @@ func _no_weapon():
 func _on_weapon_dropped(dropped_index):
 	# Set the ammo to 0 when weapon is dropped
 	ammo_label.text = "%d / %d" % [0, 0]
+	
+	match dropped_index:
+		0:
+			slot_0.texture = null
+		1:
+			slot_1.texture = null
+		2:
+			slot_2.texture = null
+		_:
+			pass
 
 func _on_weapon_switched(new_weapon, new_weapon_index):
 	# Switch the current weapon
 	weapon = new_weapon
 	index = new_weapon_index
 	update_weapon_display()
+	_highlight_slot(new_weapon_index)
 
 func _on_ammo_shot(current_ammo, given_max_ammo):
 	# Update ammo display for current weapon
 	ammo_label.text = "%d / %d" % [weapon.current_ammo, weapon.max_ammo]
+
+func _on_weapon_added(weapon:Weapon, slot:int):
+	match slot:
+		0:
+			slot_0.texture = weapon.texture
+		1:
+			slot_1.texture = weapon.texture
+		2:
+			slot_2.texture = weapon.texture
+		_:
+			pass
+
+func _highlight_slot(slot:int):
+	slot_0.modulate = Color(0.5, 0.5, 0.5, 1)
+	slot_1.modulate = Color(0.5, 0.5, 0.5, 1)
+	slot_2.modulate = Color(0.5, 0.5, 0.5, 1)
+	match slot:
+		0:
+			slot_0.modulate = Color(1, 1, 1, 1)
+		1:
+			slot_1.modulate = Color(1, 1, 1, 1)
+		2:
+			slot_2.modulate = Color(1, 1, 1, 1)
+		_:
+			pass
 
 func update_weapon_display():
 	# Highlight the selected weapon
