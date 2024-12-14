c5320ef5a0449556d3cba596ad7465af37343c1a
Noor A. <55614986+noor-ashour@users.noreply.github.com>
2024-12-13 20:20:35 -0800

    Apply theme to end UI, remove commented-out code

--- scripts/credits_button.gd
+++ scripts/credits_button.gd
@@ -2,19 +2,6 @@ extends Button
 ## Button to take player to credits
 
 func _ready():
-	#
-	#var style: StyleBoxFlat = StyleBoxFlat.new()
-	#style.bg_color = Color("#cb62ed", 0)  # Change to your desired color
-	#style.corner_radius_top_left = 8    # Optional: rounded corners
-	#style.corner_radius_top_right = 8
-	#style.corner_radius_bottom_left = 8
-	#style.corner_radius_bottom_right = 8
-	#
-	#var hover: StyleBoxFlat = StyleBoxFlat.new()
-	#hover.bg_color = Color("#cb62ed", 0.25)  # Change to your desired color
-	## Apply the style to normal state
-	#add_theme_stylebox_override("normal", style)
-	#add_theme_stylebox_override("hover", hover)
 	pass
 
 
--- scripts/restart_button.gd
+++ scripts/restart_button.gd
@@ -2,18 +2,7 @@ extends Button
 ## Button to take player back to level 1
 
 func _ready() -> void:
-	var style: StyleBoxFlat = StyleBoxFlat.new()
-	style.bg_color = Color("#cb62ed", 0)  # Change to your desired color
-	style.corner_radius_top_left = 8    # Optional: rounded corners
-	style.corner_radius_top_right = 8
-	style.corner_radius_bottom_left = 8
-	style.corner_radius_bottom_right = 8
-	
-	var hover: StyleBoxFlat = StyleBoxFlat.new()
-	hover.bg_color = Color("#cb62ed", 0.25)  # Change to your desired color
-	# Apply the style to normal state
-	add_theme_stylebox_override("normal", style)
-	add_theme_stylebox_override("hover", hover)
+	pass
 
 
 func _on_pressed() -> void:
